86fc200b59125f0243e3cbf51d53241d983552f7
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2020-11-04 18:45:16 +0200

    Remove placeholder os calls

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-os.getcwd()
-os.listdir()
 
 def move_to_folder(folder_name, root=os.getcwd()):
     try:
@@ -8,10 +6,13 @@ def move_to_folder(folder_name, root=os.getcwd()):
     except OSError:
         print(f"Invalid target path - '{root}\\{folder_name}'")
 
+
 def find_folders_with_sub_files(root=os.getcwd(), strict=False):
     subtitle_dirs = []
     check = all if strict else any
+
     for dir_path, dir_names, file_names in os.walk(root):
         if check(f.endswith(".srt") for f in file_names):
             subtitle_dirs.append(dir_path)
+   
     return subtitle_dirs
