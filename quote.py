675df47c366d99913206f59b2d2d2ace500782db
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2020-11-12 18:12:30 +0200

    Refactor quote.py
    Fix indentation and naming

--- quote.py
+++ quote.py
@@ -1,16 +1,21 @@
 class Quote:
-    def __init__(self, text, start_timestamp, end_timestamp, framerate):
+    def __init__(self, path, text, start_timestamp,
+                 end_timestamp, framerate, match_percentage):
+        self.path = path
         self.text = text
         self.start_frame = self.calc_frame(start_timestamp, framerate)
         self.end_frame = self.calc_frame(end_timestamp, framerate)
+        self.match_confidence = 0
+        
 
     def __str__(self):
         return "\n".join([self.text, str(self.start_frame), str(self.end_frame)])
+
 
     def calc_frame(self, timestamp, framerate):
         times_split = timestamp.replace(",", ":").split(":")
         times_split = [int(time) for time in times_split]
         hours, minutes, seconds, milliseconds = times_split
 
-        seconds = seconds + 60 * minutes + 3600 * hours
-        return seconds*framerate + milliseconds*framerate//1000
+        seconds_count = seconds + 60 * minutes + 3600 * hours
+        return seconds_count*framerate + milliseconds*framerate//1000
