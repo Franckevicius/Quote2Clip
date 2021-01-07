cbb14a32eec6646226c26bc1c2e55323e192a6b9
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2021-01-07 11:05:00 +0200

    Switch frame time to decimal second time

--- quote.py
+++ quote.py
@@ -1,21 +1,21 @@
 class Quote:
-    def __init__(self, path, text, start_timestamp,
-                 end_timestamp, framerate, match_percentage):
+    def __init__(self, path, text, start_timestamp, end_timestamp):
         self.path = path
         self.text = text
-        self.start_frame = self.calc_frame(start_timestamp, framerate)
-        self.end_frame = self.calc_frame(end_timestamp, framerate)
-        self.match_confidence = 0
+        self.start_time = self.calc_second_stamp(start_timestamp)
+        self.end_time = self.calc_second_stamp(end_timestamp)
+        self.sentence_match = 0
+        self.quote_match = 0
         
 
     def __str__(self):
-        return "\n".join([self.text, str(self.start_frame), str(self.end_frame)])
+        return "\n".join([self.text, str(self.start_time), str(self.end_time)])
 
 
-    def calc_frame(self, timestamp, framerate):
+    def calc_second_stamp(self, timestamp):
         times_split = timestamp.replace(",", ":").split(":")
         times_split = [int(time) for time in times_split]
         hours, minutes, seconds, milliseconds = times_split
 
         seconds_count = seconds + 60 * minutes + 3600 * hours
-        return seconds_count*framerate + milliseconds*framerate//1000
+        return f"{seconds_count}.{milliseconds}"
