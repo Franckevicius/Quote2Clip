50fe408579772394719eea5414f957a391314d31
Franckevicius <49790557+Franckevicius@users.noreply.github.com>
2021-01-07 13:16:12 +0200

    Fix video clip cutting length

--- cut_video.py
+++ cut_video.py
@@ -1,18 +1,9 @@
 import ffmpeg
 import os
-import main
 
-def calc_second_stamp(timestamp):
-    times_split = timestamp.replace(",", ":").split(":")
-    times_split = [int(time) for time in times_split]
-    hours, minutes, seconds, milliseconds = times_split
-
-    seconds_count = seconds + 60 * minutes + 3600 * hours
-    return f"{seconds_count}.{milliseconds}"
-
-start_s = calc_second_stamp("00:03:37,600")
-end_s = calc_second_stamp("00:03:40,000")
-print(start_s, end_s)
+#start_s = calc_second_stamp("00:03:37,600")
+#end_s = calc_second_stamp("00:03:40,000")
+#print(start_s, end_s)
 
 #input = ffmpeg.input("C:\Workarea\Programming\Python\Quote2Clip\Video\F02\202 - Mars University.mkv")
 #input_video = ffmpeg.trim(input, start_frame=frame_start, end_frame=frame_end)
@@ -23,16 +14,18 @@ def cut_clip(input_path, output_path, start="00.000", end="00.000"):
     video = (
         input_stream.video
         .trim(start=start, end=end)
+        .setpts('PTS-STARTPTS')
     )
     audio = (
         input_stream.audio
         .filter_('atrim', start=start, end=end)
+        .filter_('asetpts', 'PTS-STARTPTS')
     )
 
     joined = ffmpeg.concat(video, audio, v=1, a=1).node
     output = ffmpeg.output(joined[0], joined[1], output_path)
     output.run()
 
-cut_clip(input_path="C:\\Workarea\\Programming\\Python\\Quote2Clip\\Video\\F02\\202 - Mars University.mkv",
-     output_path="C:\\Workarea\\Programming\\Python\\Quote2Clip\\Output\\test.mkv",
-     start=start_s, end=end_s)
+#cut_clip(input_path="C:\\Workarea\\Programming\\Python\\Quote2Clip\\Video\\F02\\202 - Mars University.mkv",
+#     output_path="C:\\Workarea\\Programming\\Python\\Quote2Clip\\Output\\test.mkv",
+#     start=start_s, end=end_s)
